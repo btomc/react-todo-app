d54edd73e5f440fe18a6787a7ba0f7586864aee0
Beata Tomczak <tma.btty@gmail.com>
2020-09-18 23:21:00 +0200

    Basic setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="todo-app">
+      <h1>Hello Folks</h1>
+    </div>
+  );
+}
+
+export default App;
